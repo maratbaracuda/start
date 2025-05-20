2a2f1ead1326cba5ad3068b5317cc8a53759a01a
Marat <marat.saifulin@gmail.com>
2025-05-20 20:50:01 +0300

    feat: add calculate func

--- hello.py
+++ hello.py
@@ -1,12 +1,6 @@
-def print_hello():
-    print("Hello")
-class Stack():
-    def __init__(self, lst = None):
-        if lst is None:
-            lst = []
-        self.lst = lst
+def calculate(a, b):
+    return a * b
 
 if __name__ == '__main__':
-    print_hello()
-    stack = Stack([1, 2, 3])
-    print(stack.lst)
+    result = calculate(19, 20 )
+    print(result)
