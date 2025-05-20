927efa531ee76c0540b3a0a934d0ef296ac608a3
Marat <marat.saifulin@gmail.com>
2025-05-20 19:41:15 +0300

    Modify

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
 def print_hello():
     print("Hello")
+class Stack():
+    def __init__(self, lst = None):
+        if lst is None:
+            lst = []
+        self.lst = lst
 
 if __name__ == '__main__':
     print_hello()
+    stack = Stack([1, 2, 3])
+    print(stack.lst)
